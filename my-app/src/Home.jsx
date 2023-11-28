efbac6fff70c3869a715760e47162590a3bdb1df
Nimish-Dureja-10 <durejanimish14@gmail.com>
2023-11-28 10:44:42 +0530

    Redux Toolkit

---
+++ my-app/src/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {useDispatch, useSelector} from "react-redux";
+
+const Home = () => {
+    const dispatch = useDispatch();
+    const {c} = useSelector(state=>state.custom)
+
+    const addBtn = () => {
+        dispatch({
+            type : "increment"
+        });
+    }
+
+    const subBtn = () => {
+        dispatch({
+            type : "decrement"
+        })
+    }
+
+    const addBtn25 = () => {
+        dispatch({
+            type : "incrementByValue",
+            payload : 25
+        });
+    }
+
+  return (
+    <div>
+        <h2>{c}</h2>
+        <button onClick={addBtn}>Increment</button>
+        <button onClick={addBtn25}>Increment By 25</button>
+        <button onClick={subBtn}>Decrement</button>
+    </div>
+  )
+}
+
+export default Home
